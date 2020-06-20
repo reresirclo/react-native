8dced82a146b73c5c900720d5a6265e61ab5d194
Samuel Reynaldi <samuel.reynaldi@sirclo.com>
2020-06-20 15:43:20 +0700

    add ios build

--- Navigation/android/app/src/main/java/com/navigation/MainActivity.java
+++ Navigation/android/app/src/main/java/com/navigation/MainActivity.java
@@ -1,4 +1,4 @@
-package com.explore;
+package com.navigation;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Explore";
+    return "Navigation";
   }
 }
