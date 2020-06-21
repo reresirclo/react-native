53fb2b1840e8a369b7e5e8ff1c2ff167057fc2d9
Samuel Reynaldi <samuel.reynaldi@sirclo.com>
2020-06-22 02:20:56 +0700

    refactor

--- Assignment/src/component/Layout/index.js
+++ Assignment/src/component/Layout/index.js
@@ -4,7 +4,10 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const Layout = props => {
 	return (
 		<SafeAreaView
-			style={[{ flex: 1, alignItems: 'center', justifyContent: 'center' }, props.style]}>
+			style={[
+				{ flex: 1, alignItems: 'center', justifyContent: 'center' },
+				props.style,
+			]}>
 			{props.children}
 		</SafeAreaView>
 	);
