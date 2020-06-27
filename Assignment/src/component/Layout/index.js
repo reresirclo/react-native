e4c450963a1998ed6840cdbfb3bd451001220dd9
Samuel Reynaldi <samuel.reynaldi@sirclo.com>
2020-06-27 15:53:57 +0700

    change layout

--- Assignment/src/component/Layout/index.js
+++ Assignment/src/component/Layout/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Layout = (props) => {
     return (
-        <View
+        <SafeAreaView
             style={[
                 { flex: 1, alignItems: 'center', justifyContent: 'center' },
                 props.style,
             ]}>
             {props.children}
-        </View>
+        </SafeAreaView>
     );
 };
 
